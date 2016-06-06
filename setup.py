e2ded2ebddcfa62de3fc1aed878dbf508352d2c2
Guillaume Bour <guillaume@bour.cc>
2016-06-06 21:32:12 +0200

    build: update build.py (update version, add scripts, readme file name)

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 import os, sys
 from setuptools import Command, setup
 
-long_description = open('README').read()
+long_description = open('README.md').read()
 
 setup(
     author = 'Iwan Budi Kusnanto',
     url = 'https://github.com/iwanbk/nyamuk',
     author_email ='iwan.b.kusnanto@gmail.com',
-    version ='0.1',
-    install_requires = [],
+    version ='0.2.0',
     packages = ['nyamuk'],
     name = 'nyamuk',
     description = 'Python MQTT Client Library',
     long_description = long_description,
     license = "BSD",
+    scripts = ['test/pubnya.py','test/subnya.py'],
 )
